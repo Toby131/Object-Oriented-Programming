b3ec63e1f031edd6d9c83e8b878584b1979e4fc5
TOBY <leehj94131@gmail.com>
2021-01-10 14:15:34 +0900

    Add files via upload

---
+++ make_program_using_switch.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	char key;
+	cout << "Enter a key (p or q) : ";
+	cin >> key;
+	switch (key)
+	{
+	case 'p':
+		cout << "Pause" << endl;
+		break;
+	case 'P':
+		cout << "Pause" << endl;
+		break;
+	case 'q':
+		cout << "Quit" << endl;
+		break;
+	case 'Q':
+		cout << "Quit" << endl;
+		break;
+	default:
+		cout << "Default" << endl;
+	}
+	return 0;
+}
