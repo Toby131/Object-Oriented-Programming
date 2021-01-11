10c597489ec0648174e7f3679d3689f9e66c362d
TOBY <leehj94131@gmail.com>
2021-01-11 13:31:31 +0900

    Add files via upload

---
+++ conversion_string.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void change(string *str)
+{
+	cout << "input> ";
+	cin >> *str;
+}
+
+int main()
+{
+	string str = "This is default value";
+	cout << "기본값 출력> " << str << endl;
+	change(&str);
+	cout << "변환된 값 출력> " << str << endl;
+	return 0;
+}
