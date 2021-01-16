b3e6fb17af34d48b21af7c356b12f6ad38142792
TOBY <leehj94131@gmail.com>
2021-01-16 16:51:29 +0900

    Add files via upload

---
+++ account(deposit_and_withdraw).cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Account
+{
+	string name;
+	string id;
+	double balance;
+public:
+	Account()
+	{
+		name = "";
+		id = "";
+		balance = 0;
+	}
+	Account(string _name, string _id)
+	{
+		name = _name;
+		id = _id;
+		balance = 0;
+	}
+	Account(string _name, string _id, double _bal) : name(_name), id(_id), balance(_bal)
+	{
+		if (balance < 0)
+		{
+			balance = 0;
+		}
+	}
+	void deposit(double _amt) { balance += _amt; }
+	bool withdraw(double _amt)
+	{
+		if (balance - _amt < 0)
+		{
+			return false;
+		}
+		balance -= _amt;
+		return true;
+	}
+	void print()
+	{
+		cout << "name : " << name << endl;
+		cout << "id : " << id << endl;
+		cout << "balance : " << balance << endl << endl;
+	}
+};
+
+int main()
+{
+	Account ac1("배성호", "1002", 5000);
+	Account ac2;
+	ac2.print();
+	ac1.print();
+
+	int depo;
+	cout << "예금할 금액을 입력하세요 : ";
+	cin >> depo;
+	ac1.deposit(depo);
+	ac1.print();
+
+	int wdrw;
+	cout << "출금할 금액을 입력하세요 : ";
+	cin >> wdrw;
+	if (!ac1.withdraw(wdrw))
+	{
+		cout << "잔액이 부족합니다. " << endl;
+	}
+	ac1.print();
+	return 0;
+}
